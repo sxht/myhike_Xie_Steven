b78f758ed5024ca393b0aa8face2d72fb812bef1
sxht <steven.xht@gmail.com>
2023-10-31 20:28:31 -0700

    demo09 finished

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDhnLpYZjXE6RGONb3L3NRidcZ78Cyjves",
+    authDomain: "comp1800-202330-xie-steven.firebaseapp.com",
+    projectId: "comp1800-202330-xie-steven",
+    storageBucket: "comp1800-202330-xie-steven.appspot.com",
+    messagingSenderId: "440733978186",
+    appId: "1:440733978186:web:5ad2de6cdfe736d3b160b3",
+    measurementId: "G-8BWMJ0SGKT"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
